3651a2e14ba69a7e9791225c5560479a9f3a432d
pp2moonbird <pp2moonbird@gmail.com>
2017-02-06 00:00:59 +0800

    sync between 2 modes

--- static/js/anki.js
+++ static/js/anki.js
@@ -12,30 +12,7 @@ var app = new Vue({
         editFlag : false,
         editedCard : null,
         isSimpleMode: true,
-        decks: [
-            {
-                deck:'My first learning deck',
-                cards:[
-                    {
-                        question: 'what is anki',
-                        answer: 'it\'s a flaskcard system'
-                    },
-                    {
-                        question: 'what is this app',
-                        answer: 'it\'s a simple tool to generate csv for anki'
-                    }
-                ]
-            },
-            {
-                deck:'Math Dec',
-                cards:[
-                    {
-                        question: '1+1=?',
-                        answer: '2'
-                    }
-                ]
-            }
-        ],
+        decks: []
     },
 
     computed: {
@@ -45,6 +22,19 @@ var app = new Vue({
         
         compiledAnswerMarkdown: function(){
             return marked(this.answerRawText, {sanitize: true})
+        },
+
+        cardRawText: {
+            get: function(){
+                return this.questionRawText + ';' + this.answerRawText;
+            },
+            set: function(newValue){
+                var qa = newValue.split(';');
+                this.questionRawText = qa[0];
+                if(qa.length>1){
+                    this.answerRawText = qa[1];
+                }
+            }
         }
     },
 
@@ -95,31 +85,46 @@ var app = new Vue({
         addCard: function(){
             this.addFlag = true;
             this.eidtFlag = false;
-            this.editedCard = {cardRawText:'', question:'', answer:''}
+            this.editedCard = {cardRawText:'', question:'', answer:''};
+            this.enterEdit(this.editedCard);
         },
 
         editCard: function(card){
             this.addFlag = false;
             this.editFlag = true;
             card.cardRawText = card.question + ';' + card.answer;
-            this.editedCard = card;            
+            this.editedCard = card;
+            this.enterEdit(this.editedCard);            
+        },
+
+        enterEdit: function(card){
+            this.questionRawText = card.question;
+            this.answerRawText = card.answer;
         },
 
         exitEditMode: function(){
             this.editFlag = false;
             this.addFlag = false;
-            this.editedCard = null;
+            //this.editedCard = null;
         },
 
         doneEditCard: function(){
-            var card = this.editedCard;
-            var pattern = /(.+);(.+)/g;
-            var match = pattern.exec(card.cardRawText);
-            var q = match[1].trim();
-            var a = match[2].trim();
-            card.question = q;
-            card.answer = a;
-
+            if(this.isSimpleMode){
+                var card = this.editedCard;
+                var pattern = /(.+);(.+)/g;
+                var match = pattern.exec(this.cardRawText);
+                var q = match[1].trim();
+                var a = match[2].trim();
+                card.question = q;
+                card.answer = a;
+                card.cardRawText = q + ';' + a;
+            }
+            else{
+                var card = this.editedCard;
+                card.question = this.questionRawText;
+                card.answer = this.answerRawText;
+                card.cardRawText = this.cardRawText;
+            }
             if(this.addFlag){            
                 this.selectedDeck.cards.push(card);
                 this.addCard();
